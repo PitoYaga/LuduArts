04d74d07463fc2aa924138e82e24de136c1738a4
Efe Canoğlu <efe.canoglu@hotmail.com>
2026-02-03 15:00:21 +0300

    Add Item To Inventory
    You can see looted items into the inventory.

--- Assets/Scripts/UI/InventoryUI.cs
+++ Assets/Scripts/UI/InventoryUI.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryUI : MonoBehaviour
 {
@@ -16,14 +17,25 @@ public class InventoryUI : MonoBehaviour
         m_inventoryWindow.SetActive(false);
     }
 
+
+    /// <summary>
+    /// Add item to inventory. It is an text based inventory so item name is enough for now.
+    /// </summary>
+    /// <param name="itemName"></param>
     public void AddItemToInventory(string itemName)
     {
         TextMeshProUGUI spawnedSlot = Instantiate(m_InventorySlot, m_InventoryGrid);
         spawnedSlot.SetText(itemName);
+        LayoutRebuilder.ForceRebuildLayoutImmediate(m_InventoryGrid as RectTransform);
     }
 
+
+    /// <summary>
+    /// Open/Close Inventory Window
+    /// </summary>
     public void ToggleInventory()
     {
+        LayoutRebuilder.ForceRebuildLayoutImmediate(m_InventoryGrid as RectTransform);
         m_InventoryOpen = !m_InventoryOpen;
         m_inventoryWindow.SetActive(m_InventoryOpen);
     }
